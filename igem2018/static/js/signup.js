8060f2eac9208f0684fe3d220732af8bee1be078
xwy27 <xuwy27@mail2.sysu.edu.cn>
2018-10-24 12:45:00 -0400

    fixed password validation

--- igem2018/static/js/signup.js
+++ igem2018/static/js/signup.js
@@ -1,30 +1,35 @@
 $(document).ready(function () {
     
-    $('.ui.form')
-        .form({
-            fields: { 
-                username: {
-                    identifier  : 'regex',
-                    rules: [
-                      {
-                        type   : 'regExp[/^[a-zA-Z0-9]+$/]',
-                        prompt : 'Username can only contain alphas and digits.'
-                      }
-                    ]
-                  },
-                email: ['email', 'empty'],
-                password: ['minLength[6]', 'empty'],
-                password2: ['match[password]'],
-                org: ['empty'],
-                igem: ['email']
-            }
-        });
+    // $('.ui.form')
+    //     .form({
+    //         fields: { 
+    //             username: {
+    //                 identifier  : 'regex',
+    //                 rules: [
+    //                   {
+    //                     type   : 'regExp[/^[a-zA-Z0-9]+$/]',
+    //                     prompt : 'Username can only contain alphas and digits.'
+    //                   }
+    //                 ]
+    //               },
+    //             email: ['email', 'empty'],
+    //             password: ['minLength[6]', 'empty'],
+    //             password2: ['match[password]'],
+    //             org: ['empty'],
+    //             igem: ['email']
+    //         }
+    //     });
 
     $('#signup-button')
         .on('click', function () {
             var pattern = new RegExp('^[a-zA-Z0-9]+$');
             if (!pattern.test($('#username').val())) {
-                $('.error.message>.list').html('<li>Username can only contain alphas and digits.</li>');
+                $('.error.message').html('<ul class="list"><li>Username can only contain alphas and digits.</li></ul>');
+                $('.error.message').show();
+                return;
+            }
+            if ($('#password').val() != $('#password2').val() ){
+                $('.error.message').html('<ul class="list"><li>Two password are not the same.</li></ul>');
                 $('.error.message').show();
                 return;
             }
